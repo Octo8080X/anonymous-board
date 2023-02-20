8e20be8350779e1312c3a4392c55bfefb9eb4822
Octo8080 <okutannderuta0529@gmail.com>
2023-02-20 22:39:35 +0900

    update posts_remove.ts

--- anonymous-board/routes/api/posts_remove.ts
+++ anonymous-board/routes/api/posts_remove.ts
@@ -1,6 +1,6 @@
 import { HandlerContext } from "$fresh/server.ts";
 export const handler = (req: Request, _ctx: HandlerContext): Response => {
   console.log(req)
-  
-  return new Response();
+
+  return new Response("receive");
 };
