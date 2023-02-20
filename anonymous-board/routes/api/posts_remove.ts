dc13e151dbd3359b73eecb2f1158288d9640c3c6
Octo8080 <okutannderuta0529@gmail.com>
2023-02-20 23:45:08 +0900

    update

--- anonymous-board/routes/api/posts_remove.ts
+++ anonymous-board/routes/api/posts_remove.ts
@@ -1,6 +1,30 @@
 import { HandlerContext } from "$fresh/server.ts";
-export const handler = (req: Request, _ctx: HandlerContext): Response => {
-  console.log(req)
+import { envConfig } from "../../util/config.ts";
+import { Receiver } from "@upstash/qstash/mod.ts";
 
-  return new Response("receive");
+const receiver = new Receiver({
+  currentSigningKey: envConfig.UPSTASH_QSTASH_CURRENT_SIGNING_KEY,
+  nextSigningKey: envConfig.UPSTASH_QSTASH_NEXT_SIGNING_KEY,
+});
+
+export const handler = async (
+  req: Request,
+  _ctx: HandlerContext,
+): Promise<Response> => {
+
+  const isValid = await receiver.verify({
+    signature: req.headers.get("Upstash-Signature"),
+    body: await req.text(),
+  }).catch((err: Error) => {
+    console.error(err);
+    return new Response("", { status: 404 });
+  });
+
+  if (!isValid) {
+    return new Response("Invalid signature", { status: 401 });
+  }
+
+  // supabase Edge Functions を呼び出す
+
+  return new Response("", { status: 200 });
 };
